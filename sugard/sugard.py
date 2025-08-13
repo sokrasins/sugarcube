540c776ab9f4f3e53a1f30aed6132c542d904a5e
Stan <sokrasins@gmail.com>
2025-08-12 23:11:28 -0400

    Add logging and journaling

--- sugard/sugard.py
+++ sugard/sugard.py
@@ -6,8 +6,19 @@ from datetime import datetime
 import sys
 import time
 import json
+import logging
 
 import creds.tidepool_creds
+
+log = logging.getLogger("sugard")
+log.setLevel(logging.INFO)
+
+try:
+    from systemd.journal import JournalHandler
+
+    log.addHandler(JournalHandler())
+except ImportError:
+    pass
 
 cred_path = "creds/"
 endpoint = "a3tnwpx5rjol8f-ats.iot.us-east-1.amazonaws.com"
@@ -27,19 +38,19 @@ last_val = None
 
 # Callback when connection is accidentally lost.
 def on_connection_interrupted(connection, error, **kwargs):
-    print("Connection interrupted. error: {}".format(error))
+    log.error("Connection interrupted. error: {}".format(error))
 
 
 # Callback when an interrupted connection is re-established.
 def on_connection_resumed(connection, return_code, session_present, **kwargs):
-    print(
+    log.info(
         "Connection resumed. return_code: {} session_present: {}".format(
             return_code, session_present
         )
     )
 
     if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
-        print("Session did not persist. Resubscribing to existing topics...")
+        log.info("Session did not persist. Resubscribing to existing topics...")
         resubscribe_future, _ = connection.resubscribe_existing_topics()
 
         # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
@@ -49,7 +60,7 @@ def on_connection_resumed(connection, return_code, session_present, **kwargs):
 
 def on_resubscribe_complete(resubscribe_future):
     resubscribe_results = resubscribe_future.result()
-    print("Resubscribe results: {}".format(resubscribe_results))
+    log.info("Resubscribe results: {}".format(resubscribe_results))
 
     for topic, qos in resubscribe_results["topics"]:
         if qos is None:
@@ -58,13 +69,13 @@ def on_resubscribe_complete(resubscribe_future):
 
 # Callback when the subscribed topic receives a message
 def on_message_received(topic, payload, dup, qos, retain, **kwargs):
-    print("Received message from topic '{}': {}".format(topic, payload))
+    log.info("Received message from topic '{}': {}".format(topic, payload))
 
 
 # Callback when the connection successfully connects
 def on_connection_success(connection, callback_data):
     assert isinstance(callback_data, mqtt.OnConnectionSuccessData)
-    print(
+    log.info(
         "Connection Successful with return code: {} session present: {}".format(
             callback_data.return_code, callback_data.session_present
         )
@@ -74,12 +85,12 @@ def on_connection_success(connection, callback_data):
 # Callback when a connection attempt fails
 def on_connection_failure(connection, callback_data):
     assert isinstance(callback_data, mqtt.OnConnectionFailureData)
-    print("Connection failed with error code: {}".format(callback_data.error))
+    log.info("Connection failed with error code: {}".format(callback_data.error))
 
 
 # Callback when a connection has been disconnected or shutdown successfully
 def on_connection_closed(connection, callback_data):
-    print("Connection closed")
+    log.info("Connection closed")
 
 
 if __name__ == "__main__":
@@ -101,12 +112,12 @@ if __name__ == "__main__":
         on_connection_closed=on_connection_closed,
     )
 
-    print(f"Connecting to {endpoint} with client ID '{client_id}'...")
+    log.info(f"Connecting to {endpoint} with client ID '{client_id}'...")
     connect_future = mqtt_connection.connect()
 
     # Future.result() waits until a result is available
     connect_future.result()
-    print("Connected!")
+    log.info("Connected!")
 
     tp = Tidepool().connect(creds.tidepool_creds.uname, creds.tidepool_creds.passwd)
     if tp is None:
@@ -117,7 +128,7 @@ if __name__ == "__main__":
             val, timestamp = tp.bg()
             if val and timestamp:
                 # TODO: Fast update publish
-                print(
+                log.info(
                     f"{datetime.now()}: Got new bg: {round(val)} from {timestamp.isoformat()}"
                 )
 
@@ -129,20 +140,20 @@ if __name__ == "__main__":
 
                 if publish:
                     message = f'{{"value": {round(val)}, "timestamp": {timestamp.isoformat()}}}'
-                    print("    --> Publishing to '{}': {}".format(topic, message))
+                    log.info("    --> Publishing to '{}': {}".format(topic, message))
                     message_json = json.dumps(message)
                     mqtt_connection.publish(
                         topic=topic, payload=message_json, qos=mqtt.QoS.AT_LEAST_ONCE
                     )
                     last_val = val
             else:
-                print("Couldn't get bg")
+                log.warning("Couldn't get bg")
 
             time.sleep(60 * poll_period)
 
     except KeyboardInterrupt:
         # Disconnect
-        print("Disconnecting...")
+        log.info("Disconnecting...")
         disconnect_future = mqtt_connection.disconnect()
         disconnect_future.result()
-        print("Disconnected!")
+        log.info("Disconnected!")
